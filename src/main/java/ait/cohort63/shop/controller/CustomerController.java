e9f7cd9fad69c5114295f28e1a5b7da09e85b8da
Andrei <andreigulii1985@gmail.com>
2025-09-01 08:52:09 +0200

    second commit

--- src/main/java/ait/cohort63/shop/controller/CustomerController.java
+++ src/main/java/ait/cohort63/shop/controller/CustomerController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("customers")// Указывает, что контроллер обрабатывает запросы, связанные с ресурсом products
+@RequestMapping("/customers")// Указывает, что контроллер обрабатывает запросы, связанные с ресурсом products
 public class CustomerController {
 
     private final CustomerService customerService;
